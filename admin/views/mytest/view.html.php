515a392f4ac9f5ce0457fded7dbe140dc81fcf57
Amaresh Pattanaik <apattanaik@paypal.com>
2014-04-02 18:32:29 -0500

    admin files.

---
+++ admin/views/mytest/view.html.php
@@ -0,0 +1,13 @@
+<?php
+defined('_JEXEC') or die;
+
+/**
+ * HTML View class for the Login component
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_login
+ * @since       1.6
+ */
+class MytestViewMytest extends JViewLegacy
+{
+}
