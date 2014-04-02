f9acc268adc128c4e1c91de2c5f227bf9b103cd5
Amaresh Pattanaik <apattanaik@paypal.com>
2014-04-02 16:59:10 -0500

    controller.php

---
+++ admin/controller.php
@@ -0,0 +1,6 @@
+<?php
+//we start with define or die, so this PHP file can't be executed directly
+defined('_JEXEC') or die('No direct access.');
+
+class MytestController extends JControllerLegacy {
+}
