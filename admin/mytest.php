6f1b6bc885ad8635fa3476ac77af432b4157aa38
Amaresh Pattanaik <apattanaik@paypal.com>
2014-04-02 16:51:04 -0500

    New admin files.

--- admin/mytest.php
+++ admin/mytest.php
@@ -1,3 +1,12 @@
 <?php
+//we start with define or die, so this PHP file can't be executed directly
+defined('_JEXEC') or die('No direct access.');
 
-echo 'My First test component Administration.';
+//checking to make sure that the current user has a manage permission to this component
+if (!JFactory::getUser()->authorise('core.manage', 'com_mytest')) {
+	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+
+$controller  = JControllerLegacy::getInstance('Mytest');
+$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->redirect();
